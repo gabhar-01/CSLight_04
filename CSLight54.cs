9db07f151cb6e251c91723d564a9210105d73458
gabhar-01 <shevchenko.lilya@mail.ru>
2022-04-16 14:33:06 +0700

    Create CSLight54.cs

---
+++ CSLight54.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSLight54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int numResult;
+            bool isCorrect;
+
+            ConvertToInt(out userInput, out numResult, out isCorrect);
+        }
+
+        static void RequestNumber (out string userInput)
+        {
+            Console.Write("Введите строку для конвертации в число: ");
+            userInput = Console.ReadLine();
+        }
+
+        static void ConvertToInt (out string userInput, out int numResult, out bool isCorrect)
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                RequestNumber(out userInput);
+               
+                isCorrect = int.TryParse(userInput, out numResult);
+
+                if (isCorrect)
+                {
+                    Console.WriteLine("Число конвертировано корректно: " + numResult);
+                    isRunning = false;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
